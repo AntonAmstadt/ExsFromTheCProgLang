3456df44916f782d028277c6e7feb2d567557ff0
AntonAmstadt <66988031+AntonAmstadt@users.noreply.github.com>
2021-11-27 20:03:31 -0800

    Ex 2-5

--- main.c
+++ main.c
@@ -1,57 +1,34 @@
 /*
- * Ex 2-4: write an alternate version of squeeze(s1,s2) that deletes each 
- * character s1 that matches any character in the string s2.
+ * Ex 2-5 Write the function any(s1,s2), which returns the first location in the 
+ * string s1 where any character from the string s2 occurs, or -1 if s1 contains 
+ * no characters from s2. (the standard library function strpbrk does the same
+ * job but returns a pointer to the location
  */
 
 /* 
  * File:   main.c
  * Author: Anton
  *
- * Created on November 27, 2021, 7:12 PM
+ * Created on November 27, 2021, 7:52 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-// the squeeze is from the book
-// squeeze: delete all c from s
-void squeeze(char s[], int c) {
+int any(char s1[], char s2[]){
     int i, j;
-    
-    for (i = j = 0; s[i] != '\0'; i++)
-        if (s[i] != c)
-            s[j++] = s[i];
-    s[j] = '\0';
-}
-
-// my function
-// meant for use with short strings, not an efficient algorithm for long strings O(n^2)
-void squeeze2(char s1[], char s2[]){
-    int i, j, found;
-    int k = 0;
-    
-    for (i = 0; s1[i] != '\0'; i++){
-        found = 0;
+    for(i = 0; s1[i] != '\0'; i++){
         for(j = 0; s2[j] != '\0'; j++){
-            if(s1[i] == s2[j]){
-                found = 1;
-                break; // saves some time if s2 is long and repeats characters
-            }   
+            if(s1[i] == s2[j])
+                return i;
         }
-        if(!found) 
-            s1[k++] = s1[i];
     }
-    s1[k] = '\0';
+    return -1;
 }
-
 int main(int argc, char** argv) {
-    //test my function
     char s1[] = {'a', 'n', 't', 'o', 'n', '\0' };
-    char s2[] = {'n', 'o', '\0' };
-    squeeze2(s1, s2);
-    for(int i = 0; s1[i] != '\0'; i++){
-        putchar(s1[i]);
-    }
+    char s2[] = {'t', 'o', '\0' };
+    printf("%d", any(s1, s2));
 
     return (EXIT_SUCCESS);
 }
