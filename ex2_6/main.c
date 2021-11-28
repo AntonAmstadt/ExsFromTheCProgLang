e336943f76de91ff9af63c30adc760b740dad85b
AntonAmstadt <66988031+AntonAmstadt@users.noreply.github.com>
2021-11-27 21:27:58 -0800

    2-6 bitwise

--- ex2_6/main.c
+++ ex2_6/main.c
@@ -1,8 +1,7 @@
 /* 
  * Ex 2-6
  * Write a function setbits(x,p,n,y) that returns x with the n bits that begin
- * at position p set to the rightmost n bits of y, leaving the other bits unchanged.
- * p = 0 at the least significant bit.
+ * at position p set to the rightmost n bits of y, leaving the other bits unchanged
  */
 
 /* 
