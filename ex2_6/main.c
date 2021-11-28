6801e11b28e72a59b6ff1821c65afbdc23a4f2b5
AntonAmstadt <66988031+AntonAmstadt@users.noreply.github.com>
2021-11-27 21:23:19 -0800

    fun

--- ex2_6/main.c
+++ ex2_6/main.c
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
     // test here
     unsigned a = 0xF;
     unsigned b = 0;
-    printf("%d", setbits(a, 2, 2, b));
+    printf("%u", setbits(a, 2, 2, b));
     return (EXIT_SUCCESS);
 }
 
